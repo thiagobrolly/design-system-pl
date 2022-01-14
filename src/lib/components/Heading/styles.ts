9718c33a5b268a26548fcdf89a77394c07f95db3
Thiago Sousa <thiago.sousa@paylivre.com>
2022-01-14 15:07:59 -0300

    Adicionando cores ao tema padrão e documentação

--- src/lib/components/Heading/styles.ts
+++ src/lib/components/Heading/styles.ts
@@ -4,22 +4,22 @@ import { theme } from '../../styles/theme';
 
 const mediaFont = () => css`
   @media ${theme.media.lteMedium} {
-    font-size: ${theme.font.sizes.xlarge};
+    font-size: ${theme.font.size.l3};
   }
 `;
 
 const titleSize = {
   small: () => css`
-    font-size: ${theme.font.sizes.small};
+    font-size: ${theme.font.size.s1};
   `,
   medium: () => css`
-    font-size: ${theme.font.sizes.medium};
+    font-size: ${theme.font.size.m1};
   `,
   big: () => css`
-    font-size: ${theme.font.sizes.large};
+    font-size: ${theme.font.size.l1};
   `,
   huge: () => css`
-    font-size: ${theme.font.sizes.huge};
+    font-size: ${theme.font.size.l3};
     ${mediaFont()};
   `,
 };
@@ -30,7 +30,7 @@ const titleCase = (uppercase: boolean) => css`
 
 export const Title = styled.h1<HeadingProps>`
   ${({ colorDark, size, uppercase }) => css`
-    color: ${colorDark ? theme.colors.secondaryA : theme.colors.white};
+    color: ${colorDark ? theme.color.secondary_variant : theme.color.white};
     line-height: 1.2;
     ${titleSize[size || 'small']()};
     ${titleCase(uppercase || false)};
